7606ecd50b1e744c66426e5b6fa4081c0b06d697
Joshua <joshua.jaup@hotmail.com>
2024-10-15 16:00:27 +0200

    Implemented Google Analytics and Google Tag Manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,55 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import MyListPage from "./pages/MyListPage";
 import MovieDetailsPage from "./pages/MovieDetailsPage";
 import Footer from "./components/Footer"; // Importera Footer
 import { Helmet } from 'react-helmet'; // Importerar Helmet
+import ReactGA from "react-ga4"; // Importera Google Analytics
+
+const TRACKING_ID = "G-YF2XHQB511"; // Ditt Google Analytics mät-ID
 
 function App() {
+  const location = useLocation();
+
+  // Initiera Google Analytics
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID); // Initiera GA
+    ReactGA.send("pageview"); // Spåra sidvisning
+  }, []);
+
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearchChange = (term) => {
     setSearchTerm(term);
   };
 
+  // Spåra sidvisningar vid navigering
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
   return (
-    <Router>
+    <div className="flex flex-col min-h-screen bg-gray-900"> {/* Flexbox layout */}
       <Helmet>
         <title>JJ Streaming</title>
         <meta name="description" content="Discover the most popular movies and search for your favorites."/>
         <meta name="keywords" content="movies, popular movies, movie library,"/>
       </Helmet>
-      <div className="flex flex-col min-h-screen bg-gray-900">{" "} {/* Flexbox layout */}
-        <Header onSearchChange={handleSearchChange} />
-        <main className="flex-grow">{" "}{/* Innehållet växer för att fylla utrymmet */}
-          <Routes>
-            <Route path="/" element={<Home searchTerm={searchTerm} />} />
-            <Route path="/my-list" element={<MyListPage />} />
-            <Route path="/movie/:id" element={<MovieDetailsPage />} />
-          </Routes>
-        </main>
-        <Footer className="mt-auto" />{" "}
-      </div>
-    </Router>
+      <Header onSearchChange={handleSearchChange} />
+      <main className="flex-grow"> {/* Innehållet växer för att fylla utrymmet */}
+        <Routes>
+          <Route path="/" element={<Home searchTerm={searchTerm} />} />
+          <Route path="/my-list" element={<MyListPage />} />
+          <Route path="/movie/:id" element={<MovieDetailsPage />} />
+        </Routes>
+      </main>
+      <Footer className="mt-auto" />
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store'; // Importera Redux store
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // Importera BrowserRouter
 import './index.css';
+import TagManager from "react-gtm-module"
+
+const tagManagerArgs = {
+  gtmId: "GTM-TZRFV83N",
+};
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter> {/* Omge App-komponenten med BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 );
